4ed325933b37576c3f3eaea33e18a0701e5339ae
Gaohaoyang <gaohaoyang126@126.com>
2015-03-29 00:18:37 +0800

    bug

--- js/index.js
+++ js/index.js
@@ -9,6 +9,9 @@ $(document).ready(function() {
 			'left': l+'px'
 		}).animate({'top':146,'opacity':1}, 300);
 	}, function() {
-		$('.tip').animate({'top': 100, 'opacity': 0}, 200);
+		/* Here still is a bug, I don't know how to fix it */
+		if (!$('.tip').is(':animated')) {
+			$('.tip').animate({'top': 100, 'opacity': 0}, 200);
+		};
 	});
 });
